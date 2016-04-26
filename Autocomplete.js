d20455658c7888fd310ddab375b9e113ed1f4fe8
akopichin <akopichin@gmail.com>
2016-04-26 03:25:18 +0300

    stuck (

--- Autocomplete.js
+++ Autocomplete.js
@@ -1,6 +1,36 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import './style.scss';
+import linkState from 'react-link-state';
+
+class MyForm extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+          searchText: ''
+      }
+
+    }
+
+    render() {
+        return (
+            <form action=''>
+                form searchText: {this.state.searchText}<br />
+                <Autocomplete 
+                    ref="mycomplete"
+                    valueLink={linkState(this, 'searchText')} 
+                    fetch={(search) => new Promise((resolve) => 
+                            {setTimeout(() =>  resolve(
+                                [{ id: 1, name: 'one' }, 
+                                 { id: 2, name: 'two'} ]
+                             ), 1000); } ) } 
+                    itemRender={(item) => <AutocompleteItem key={item.id} ref={item.id} item={item} />}
+                />
+            </form>
+        ) 
+    }
+}
 
 class Autocomplete extends React.Component {
     constructor(props) {
@@ -15,29 +45,31 @@ class Autocomplete extends React.Component {
 
       this.aUpdate = this.aUpdate.bind(this);
       this.aItemSelect = this.aItemSelect.bind(this);
-      this.fetch = this.fetch.bind(this);
     }
 
     aUpdate(e){
         let searchText = e.target.value;
         this.setState({searchText: searchText});
 
-        let items = this.fetch(searchText);
+        let items = this.props.fetch(searchText);
         
         items.then(result => this.setState({showDropdown: true, items: result}) );
     }
 
-    fetch(val) {
-        // val is our searchText so pass to search query - in our case just replaced with timeout
-        return new Promise(
-            (resolve) => {
-                setTimeout(() =>  resolve([{ id: 1, name: 'one' }, { id: 2, name: 'two'} ]), 1000);
-            }
-        );
-    }
-
     aItemSelect(itemid, itemtext){
         this.setState({ showDropdown: false, searchText: itemtext, searchId: itemid });
+        this.props.valueLink.requestChange(itemtext); // or id?
+    }
+
+    componentDidUpdate(){
+        let iSel = this.aItemSelect;
+        if ($('.autocomplete-dropdown .autocomplete-dropdown-item').size() > 0){
+            $('.autocomplete-dropdown .autocomplete-dropdown-item').each(function(){
+                $(this).off().on('click', function() {
+                    iSel($(this).html());
+                });
+            })
+        }
     }
 
     render() {
@@ -46,14 +78,17 @@ class Autocomplete extends React.Component {
         let aItemSelect = this.aItemSelect;
         let showDropdown = this.state.showDropdown;
 
+/*
         let items = this.state.items.map( item =>  {
-            return <DropdownItem 
+            return <AutocompleteItem 
                 key={item.id} 
-                itemid={item.id} 
-                itemtext={item.name} 
-                onSelect={aItemSelect.bind(this, item.id, item.name)} 
+                item={item} 
+                //onSelect={aItemSelect.bind(this, item)} 
                 />;
         });
+        */
+
+        let items = this.state.items.map(this.props.itemRender);
 
         return (
             <div>
@@ -87,24 +122,31 @@ Autocomplete.defaultProps = {
 
 /*  item */
 
-class DropdownItem extends React.Component {
+class AutocompleteItem extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+
     render() {
-        let id = this.props.itemid;
-        let text = this.props.itemtext;
-        let onSelect = this.props.onSelect;
+        let id = this.props.item.id;
+        let text = this.props.item.name;
+        //let onSelect = this.props.onSelect;
+        //console.log(this.props);
 
         return (
-            <div className="autocomplete-dropdown-item" itemid={id} onClick={onSelect}>{text}</div>
+            <div className="autocomplete-dropdown-item" itemid={id} onClick={this.props.onSelect} >{text}</div>
         )
     }
 }
+
+//<div className="autocomplete-dropdown-item" itemid={id} onClick={onSelect}>{text}</div>
 
 
 
 /*  render */
 
 ReactDom.render(
-    <Autocomplete />,
+    <MyForm />,
     document.getElementById('app')
 );
 
